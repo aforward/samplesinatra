2c78e8d1633fd17d7fad96de2f575dee388882f2
Andrew Forward <aforward@gmail.com>
2017-01-21 08:52:52 -0500

    Add post and param example

--- server.rb
+++ server.rb
@@ -4,3 +4,8 @@ get '/' do
   @view = {title: "Hello World", h1: "Welcome to ERB"}
   erb :'hello/world', :layout => :home
 end
+
+post '/' do
+  @view = {title: params[:newTitle], h1: params[:newH1]}
+  erb :'hello/world', :layout => :home
+end
